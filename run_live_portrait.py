810c4df80b8d1d58b08c98e95e55d722a279e7ec
Theodore <tinprocoder0908@gmail.com>
2024-07-20 13:47:55 +0700

    TensorRT availabled

--- run_live_portrait.py
+++ run_live_portrait.py
@@ -16,7 +16,7 @@ def main(video_path, source_img, use_tensorrt, real_time, half):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Live Portrait Rendering Script')
-    parser.add_argument('-v', '--video', '--video_path_or_webcam_id', type=str, required=True,
+    parser.add_argument('-v', '--video', type=str, required=True,
                         help='Path to the driving video or your webcam id')
     parser.add_argument('-i', '--image', type=str, required=True, help='Path to the source image')
     parser.add_argument('-e', '--run_time', action='store_true', help='Turn on TensorRT mode')
@@ -24,4 +24,4 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--real_time', action='store_true', help='Enable real-time webcam demo')
     args = parser.parse_args()
 
-    main(args.video_path_or_webcam_id, args.source_img, args.use_tensorrt, args.half_precision, args.real_time)
+    main(args.video, args.image, args.run_time, args.half_precision, args.real_time)
